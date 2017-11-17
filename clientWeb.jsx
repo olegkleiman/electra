94921f378a49af412769a1b24c2e981e3614ef0e
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-17 12:40:14 +0200

    Update clientWeb.jsx

--- clientWeb.jsx
+++ clientWeb.jsx
@@ -9,13 +9,6 @@ import firebase from 'firebase';
 import reducers from './reducers.jsx';
 import App from './app/App.jsx';
 
-var config = {
-  databaseURL: 'https://electra-fc7c5.firebaseio.com/',
-};
-var defaultApp = firebase.initializeApp(config);
-var db = defaultApp.database();
-
-var refSubs = db.ref('subs');
 
 const store = createStore(reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
@@ -34,7 +27,41 @@ if( preloadedState ) {
 
 }
 
+var config = {
+  apiKey: 'AIzaSyBpOJXFoIcXOi-WV5OpUV5Pr5FeRQ1eoiI',
+  databaseURL: 'https://electra-fc7c5.firebaseio.com/',
+};
+var defaultApp = firebase.initializeApp(config);
+var dbRef = defaultApp.database().ref();
+
+var Subs = dbRef.child('actions');
+
+// Enumerate subscriptions for 'actions' root - only once!
+Subs.once('value', (snap) => {
+
+    snap.forEach( (item) => {
+
+      console.log('Subscription enumerated: ' + item.key);
+
+      // For each subscription found, establish listener for added actions
+      Subs.child(item.key).on('child_added', (s) => {
+
+          let _val = s.val();
+          console.log('Action reported for ' + item.key + ':');
+          console.log(_val);
+
+          store.dispatch({
+            type: 'FS_EVENT',
+            data: _val
+          });
+
+      });
+
+    });
+
+});
+
 ReactDOM.hydrate(<Provider store={store}>
-                  <App fbRef={refSubs}/>
+                  <App />
                 </Provider>,
                 document.getElementById('app'));
