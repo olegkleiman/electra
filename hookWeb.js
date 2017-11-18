9ccf24f9bc306dc6dbe30dd35db70996e8afa484
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-19 00:38:22 +0200

    Update hookWeb.js

--- hookWeb.js
+++ hookWeb.js
@@ -1,3 +1,4 @@
 require("babel-core/register");
+['.css', '.oft', '.ttf', '.woff', '.woff2'].forEach( (ext) => require.extensions[ext] = () => {} );
 require("babel-polyfill");
 require("./serverExpress.js")
