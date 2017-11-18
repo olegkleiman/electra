a745f5ad2a1f4145e4039a9c5deb9ebc1e99dc82
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-19 00:45:06 +0200

    Update EventsList.jsx

--- app/EventsList.jsx
+++ app/EventsList.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import {
-    Accordion,
-    AccordionItem,
-    AccordionItemTitle,
-    AccordionItemBody,
-} from 'react-accessible-accordion';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+
+import { Accordion, AccordionItem } from 'react-sanfona';
+
+import styles from '../css/eventslist.css';
 
 class EventsList extends React.Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (<Accordion>
-      <AccordionItem>
-        <AccordionItemTitle>
-          <h3>Simple title</h3>
-        </AccordionItemTitle>
-        <AccordionItemBody>
-          <p>Body content</p>
-        </AccordionItemBody>
-      </AccordionItem>
-    </Accordion>)
+    return (<div className={styles.container}>
+              <Accordion>
+                <AccordionItem className='accordion_item' title='25.10.2017'>
+                  One
+                </AccordionItem>
+                <AccordionItem className='accordion_item' title='26.10.2017'>
+                  Two
+                </AccordionItem>
+              </Accordion>
+            </div>)
   }
 
 };
 
-export default EventsList;
+const mapStateToProps = state => {
+
+  return {
+    fsEvent: state.fsEvent,
+    fsEventSubscription: state.fsEventSubscription,
+    monitors: state.monitors,
+    lastSubscription: state.lastSubscription,
+    lastSubscriptionTime: state.lastSubscriptionTime
+  };
+};
+
+export default connect(mapStateToProps)(EventsList);
