3eb1b3c10802534867be6e0cdf84f995c0d41e92
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-18 17:45:40 +0200

    Update App.test.js

--- test/App.test.js
+++ test/App.test.js
@@ -3,6 +3,10 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import { configure } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import reducers from '../reducers.jsx';
 import App from '../app/App.jsx';
 
@@ -12,6 +16,9 @@ test('Test App component', () => {
   const component = renderer.create(<Provider store={store}>
                                       <App />
                                     </Provider>);
+  // Configure Enzyme before using it
+  //configure({ adapter: new Adapter() });
+  //const component = shallow(<App />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
