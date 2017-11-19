70995b6777c1024ed33ed7d42c1a0d4f09678e96
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-19 03:40:08 +0200

    Update EventsList.jsx

--- app/EventsList.jsx
+++ app/EventsList.jsx
@@ -6,17 +6,48 @@ import { Accordion, AccordionItem } from 'react-sanfona';
 
 import styles from '../css/eventslist.css';
 
+// Use named export for unconnected component for jest
+//export
 class EventsList extends React.Component {
 
   constructor(props) {
     super(props);
   }
 
+  componentWillReceiveProps(nextProps){
+    if( nextProps.fsEvent &&
+        nextProps.fsEventSubscription) {
+
+      const currentMonitor = this.props.monitors.find( (monitor) => {
+        return monitor.subscriptionName === nextProps.fsEventSubscription
+      });
+
+      if( currentMonitor )
+        currentMonitor.notifications++;
+
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if( nextProps.lastSubscription === '' ) {
+      return false;
+    } else {
+        this.props.monitors.forEach( (monitor, index) => {
+          if( monitor.subscriptionName === nextProps.lastSubscription )
+              this.props.monitors[index].notifications++;
+        });
+
+        return true;
+    }
+  }
+
   render() {
+
     return (<div className={styles.container}>
+              <h1>Sanfona</h1>
               <Accordion>
                 <AccordionItem className='accordion_item' title='25.10.2017'>
-                  One
+                  <h2>One</h2>
                 </AccordionItem>
                 <AccordionItem className='accordion_item' title='26.10.2017'>
                   Two
