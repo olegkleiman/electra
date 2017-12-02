797d92a9025f1b5e9f1e0691f41c3759b434497d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-12-03 00:53:12 +0200

    Update MonitorSelector.jsx

--- app/MonitorSelector.jsx
+++ app/MonitorSelector.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Dropdown } from 'muicss/react'
+import { Dropdown, DropdownItem } from 'muicss/react';
 
 class MonitorSelector extends React.Component {
 
   constructor(props) {
+
     super(props);
+
+    this.state = {
+      activeSubscription: 'select monitor'
+    };
+
+    this.onMonitorSelected = this.onMonitorSelected.bind(this);
   }
 
-  onMonitorSelected = (e) => {
-    e.preventDefault();
+  onMonitorSelected(e) {
+
+    //e.preventDefault();
+    const index = parseInt(e, 10);
+    const _monitor = this.props.monitors[index];
+
+    this.setState({
+      activeSubscription: _monitor.subscriptionName
+    })
+
+    this.props.dispatch({
+      type: 'ACTIVE_MONITOR',
+      data: {
+        subscriptionName: _monitor.subscriptionName,
+        folderName: _monitor.folder
+      }
+    })
   }
 
   render() {
-    return (<Dropdown color='primary' lalbel='select monitor'
+    return (<Dropdown color='primary' label={this.state.activeSubscription}
                       onSelect={this.onMonitorSelected}>
               { this.props.monitors.map( (monitor, index) => {
-                  return <DropdownItem key={index}>
-                    sss
+                  return <DropdownItem key={index} value={index}>
+                    {monitor.subscriptionName}
                   </DropdownItem>
                 })
               }
