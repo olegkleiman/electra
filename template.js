66a41a4d1484cb6bacff00046e90ce0bbe99a28f
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-12-03 00:59:30 +0200

    Update template.js

--- template.js
+++ template.js
@@ -1,12 +1,14 @@
 export default ({content, state}) => {
   return `
       <!DOCTYPE html>
-      <html dir="rtl" lang="he">
+      <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, shrink-to-fit=no" />
         <title>Electra Monitor</title>
         <link href='sanfona.css' rel='stylesheet' />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link href="//cdn.muicss.com/mui-0.9.30/css/mui.min.css" rel="stylesheet" type="text/css" />
         <link href='http://localhost:8080/styles.css' rel='stylesheet' />
        </head>
       <body>
@@ -14,6 +16,9 @@ export default ({content, state}) => {
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(state)};
         </script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>        
         <script id='bundle' src="http://localhost:8080/bundle.js"></script>
       </body>
       </html>
